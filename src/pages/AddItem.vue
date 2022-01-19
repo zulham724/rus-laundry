d005471aa08e27a0ec65f63f340d2e3e2ddcfe6f
Zulham Azwar Achmad <zulham724@students.unnes.ac.id>
2022-01-19 08:14:38 +0700

    updateupdate

---
+++ src/pages/AddItem.vue
@@ -0,0 +1,31 @@
+<template>
+  <q-layout class="mbl" view="lHh lpR fFf" style="background-color: #fafafa">
+    <q-page-container
+      style="background-image: linear-gradient(to top left, #179ce7, #a3f2f1)"
+    >
+      <q-page>
+        <div class="text-center"></div>
+
+        <div
+          class="front q-pb-lg"
+          style="
+            border-radius: 20px 20px 0 0;
+            background-color: #fafafa;
+            margin-top: -30px;
+            position: relative;
+          "
+        >
+          <div class="text-weight-bold text-h6 q-pt-xl q-pl-md q-pb-md">
+            Kategori
+          </div>
+        </div>
+      </q-page>
+    </q-page-container>
+  </q-layout>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
