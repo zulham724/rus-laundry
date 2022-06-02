59cb3378c78c12e73715ebd3e98dbc565f06789a
Aryosetowmn <aryosetowmnwork@gmail.com>
2022-06-02 09:00:54 +0700

    update

--- public/config.js
+++ public/config.js
@@ -2,8 +2,8 @@
 // const API_URL = "http://localhost:8000";
 // const STORAGE_URL = "http://localhost:8000/storage";
 
-const API_URL = "http://localhost:8000";
-// const API_URL = "https://api.laundrydigital.co.id";
+// const API_URL = "http://localhost:8000";
+const API_URL = "https://api.laundrydigital.co.id";
 const APP_URL = "https://apps.laundrydigital.co.id";
 const STORAGE_URL = "https://cdn-agpaiidigital.online/laundrydigital";
 // const API_URL = "https://api.laundrydigital.co.id";/
